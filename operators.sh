d5b7bdd9fe3ac48d93a4016f26c7cd06e5264df5
pratikktiwari <ipratiktiwari@gmail.com>
2021-02-24 00:04:28 +0530

    Bash

---
+++ operators.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+X=3
+Y=$((100 * $X + 5)) #305
+echo $Y #305
+
+#Mathematical operators
+# x + y => addition
+# a - b => subtraction
+# a * b => multiply
+# a / b => division
+# a % b => modulo
+# a ** b => a raised to b
